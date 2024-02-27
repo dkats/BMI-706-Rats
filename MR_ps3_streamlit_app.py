bc4062a608be4b706ced966168f88d156e7f94df
mondira17 <mondiraray@gmail.com>
2024-02-27 16:58:59 -0500

    1.5

--- MR_ps3_streamlit_app.py
+++ MR_ps3_streamlit_app.py
@@ -29,7 +29,7 @@ percentiles_df = pd.DataFrame({
     'Label': ['50th', '90th', '95th']
 })
 
-percentile_lines = alt.Chart(percentiles_df).mark_rule(color='black', size=2).encode(  # Set size to control line thickness
+percentile_lines = alt.Chart(percentiles_df).mark_rule(color='black', size=1.5).encode(  # Set size to control line thickness
     y='Percentile:Q'
 )
 
