574adfcb16d0b1ac1f6dd0abbc36c8f0834baaf8
mondira17 <mondiraray@gmail.com>
2024-02-27 16:54:51 -0500

    343

--- MR_ps3_streamlit_app.py
+++ MR_ps3_streamlit_app.py
@@ -40,7 +40,7 @@ percentile_labels = percentile_lines.mark_text(
     dy=-5,
     text='Label:N'
 ).encode(
-    x=alt.value(340),
+    x=alt.value(343),
     y='Percentile:Q',
     text='Label:N'
 )
