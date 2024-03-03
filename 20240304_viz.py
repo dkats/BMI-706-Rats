70b118af2cbf5f6175ec6b2abdb78f065fb54275
mondira17 <mondiraray@gmail.com>
2024-03-03 14:57:53 -0500

    color

--- 20240304_viz.py
+++ 20240304_viz.py
@@ -20,37 +20,8 @@ diastolic_percentile = diastolic_bp + age - height # Example value
 data = pd.DataFrame({
     'Age': [age, age],
     'Percentile': [systolic_percentile, diastolic_percentile],
-    'Type': ['Systolic BP', 'Diastolic BP'],
-    'Blood Pressure Value': [systolic_bp, diastolic_bp]  # Include actual BP values here
+    'Type': ['Systolic BP', 'Diastolic BP']
 })
-
-# Define a color scale for the percentiles
-def get_color(percentile):
-    if percentile >= 95:
-        return 'red'
-    elif percentile >= 90:
-        return 'orange'
-    elif percentile > 50:
-        return 'green'
-    else:
-        return 'lightgrey'
-
-# Apply the color scale to the DataFrame
-data['Color'] = data['Percentile'].apply(get_color)
-
-# Chart for colored circles
-colored_circles = alt.Chart(data).mark_circle(
-    size=300,  # Adjust circle size as needed
-).encode(
-    x=alt.X('Age:Q', title='Age (years)', scale=alt.Scale(domain=(0, 13)), axis=alt.Axis(tickCount=13)),
-    y=alt.Y('Percentile:Q', title='Percentile'),
-    color=alt.Color('Color:N', legend=None),  # Use the color encoding
-    tooltip=[
-        alt.Tooltip('Type:N', title='Blood Pressure Type'),
-        alt.Tooltip('Blood Pressure Value:Q', title='Blood Pressure Value'),  # Include BP value in tooltip
-        alt.Tooltip('Percentile:Q', title='Percentile')
-    ]
-)
 
 # Define horizontal lines for the 50th, 90th, and 95th percentiles
 percentiles_df = pd.DataFrame({
@@ -69,14 +40,29 @@ percentile_labels = percentile_lines.mark_text(
     dy=-5,
     text='Label:N'
 ).encode(
+    x=alt.value(344.5),
     y='Percentile:Q',
     text='Label:N'
 )
 
+# Adding a calculated field for color based on conditions
+data['Color'] = data['Percentile'].apply(lambda x: 'red' if x >= 95 else ('yellow' if x >= 90 else ('green' if x > 50 else 'blue')))
+
+# Base chart for points with conditional coloring based on the new 'Color' field
+points = alt.Chart(data).mark_point(
+    filled=True,
+    size=100
+).encode(
+    x=alt.X('Age:Q', title='Age (years)', axis=alt.Axis(values=list(range(14))), scale=alt.Scale(domain=(0, 13))),
+    y=alt.Y('Percentile:Q', title='Percentile', scale=alt.Scale(domain=(0, 100))),
+    color=alt.Color('Color:N', legend=alt.Legend(title='Percentile Color'), scale=None),  # Directly use the 'Color' field
+    tooltip=['Type', 'Percentile']
+)
+
 # Combine all chart layers
 chart = alt.layer(
-    colored_circles,
-    percentile_lines,
+    points, 
+    percentile_lines, 
     percentile_labels
 ).properties(
     title='',
@@ -84,6 +70,10 @@ chart = alt.layer(
     height=350
 ).configure_view(
     strokeWidth=0
+).configure_axis(
+    labelPadding=5,
+    titlePadding=5,
+    grid=False
 )
 
 # Display the chart in Streamlit
