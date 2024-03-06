d077a7a93e1478e6c64999107c37fa3679a7d5f8
mondira17 <mondiraray@gmail.com>
2024-03-05 19:31:08 -0500

    revert defaults

--- 20240305_viz.py
+++ 20240305_viz.py
@@ -28,9 +28,9 @@ if patient_id == 'Select a patient OR input values below':
     # User inputs
     sex = st.radio('Select sex:', ('Male', 'Female'))
     age = st.slider('Select age (years):', min_value=0, max_value=13, value=10)
-    height = st.number_input('Enter height (cm):', value=0, format="%d")
-    systolic_bp = st.number_input('Enter systolic blood pressure (mmHg):', value=0, format="%d")
-    diastolic_bp = st.number_input('Enter diastolic blood pressure (mmHg):', value=0, format="%d")
+    height = st.number_input('Enter height (cm):', min_value=0, value=50)
+    systolic_bp = st.number_input('Enter systolic blood pressure (mmHg):', min_value=0, value=96)
+    diastolic_bp = st.number_input('Enter diastolic blood pressure (mmHg):', min_value=0, value=60)
 
     # Create a DataFrame for user inputs
     data = pd.DataFrame({
