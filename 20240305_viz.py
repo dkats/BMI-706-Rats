ae066ca9f09dfa846e401500bb2e020081bfa328
mondira17 <mondiraray@gmail.com>
2024-03-05 20:35:33 -0500

    title

--- 20240305_viz.py
+++ 20240305_viz.py
@@ -15,7 +15,7 @@ pertable_md = pd.read_csv('bp-tables/MaleDBP.csv')
 pertable_ms = pd.read_csv('bp-tables/MaleSBP.csv')
 
 # Streamlit app setup
-st.title('Pediatric Blood Pressure Percentiles for Screening and Management of High Blood Pressure')
+st.title('Pediatric Blood Pressure Percentiles for Screening and Management of High Blood Pressure (0-13 years old)')
 
 # Load NHANES
 nhanes = pd.read_csv('nhanes/nhanes_clean.csv')
